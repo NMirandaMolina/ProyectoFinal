f156cb7768e4a45d46872490a89a144e52cdceb3
Gustavo Infante <dubaninfante04@gmail.com>
2024-11-17 22:12:03 -0500

    Creacion de la clase RegistroEntrada

---
+++ src/RegistroEntrada.java
@@ -0,0 +1,13 @@
+public class RegistroEntrada {
+    public RegistroEntrada(Computador computador, String fecha) {
+        super(computador, fecha);
+    }
+
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Registro de Entrada:");
+        computador.mostrarInfo();
+        System.out.println("Fecha de entrada: " + fecha);
+    }
+
+}
